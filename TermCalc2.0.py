d3e4f089f4eab1fe52961e89ec90163d91e07834
Lucas <49869362+CodingLucky@users.noreply.github.com>
2019-04-22 21:13:27 +0200

    Add files via upload

---
+++ TermCalc2.0.py
@@ -0,0 +1,23 @@
+while True:
+    print("Welcome to TermCalc, the Terminal-Based Calculator.")
+
+    # Input Types #
+
+    n1 = float(input("First Number: "))
+    op = input("Which Operator?(+, -, *, /, %): ")
+    n2 = float(input("Second Number: "))
+
+    # Define what to do #
+    if op == "+":
+        print(n1 + n2)
+    elif op == "-":
+        print(n1 - n2)
+    elif op == "/":
+        print(n1 / n2)
+    elif op == "*":
+        print(n1 * n2)
+    elif op == "%":
+        print((n1 / n2 * 100.0))
+
+    else:
+        print("Invalid Input, please type a right operator!")
